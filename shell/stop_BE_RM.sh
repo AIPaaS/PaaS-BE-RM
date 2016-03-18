3eee62d1e04d7dce6ed20c8d6747937fdcc70e07
yuanman <yuanman@asiainfo.com>
2016-03-18 20:55:32 +0800

    update stop_BE_RM.sh

--- shell/stop_BE_RM.sh
+++ shell/stop_BE_RM.sh
@@ -1,1 +1,1 @@
-/usr/bin/ps -ef |grep PaaS-BE-RM|grep -v "grep"|awk '{print $2}'|xargs kill -9
+/usr/bin/ps -ef |grep PaaS-BE-RM |grep DubboServiceStart|grep -v "grep"|awk '{print $2}'|xargs kill -9
